f60d33042aec30d14aa6ce4b4265f605571376c7
adalhub <adalteran@gmail.com>
2025-05-29 16:25:11 -0500

    random subset creator:function added for uniform probability across k choises from set

--- randNum_subset.py
+++ randNum_subset.py
@@ -7,3 +7,26 @@ def genRandSubSet(n,k):
         result.append(randNum)
     return result
 print(genRandSubSet(100,4))
+
+
+def random_subset(n: int, k: int):
+
+    #Uniform random subset of size k from {0..n-1}
+    #O(k) time / O(k) extra space.
+
+    changed = {}                    #maps index to value actually stored there
+    for i in range(k):
+        r = random.randrange(i, n)  #chooses partner to swap with i  
+
+        #logical values currently at i and r
+        val_r = changed.get(r, r)
+        val_i = changed.get(i, i)
+
+        #perform the logical swap
+        changed[i] = val_r
+        changed[r] = val_i
+
+    return [changed[i] for i in range(k)]
+
+print(random_subset(5, 3))   #tests
+print(random_subset(1000000, 5))  
