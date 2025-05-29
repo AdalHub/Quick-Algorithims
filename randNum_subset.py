a1e576571bdf6bbc6d2731c12bb522caca807ecc
adalhub <adalteran@gmail.com>
2025-05-29 17:09:51 -0500

    edge case update for random_subset generator

--- randNum_subset.py
+++ randNum_subset.py
@@ -1,20 +1,26 @@
 import random
 
 def genRandSubSet(n,k):
+    #Just a random number generator from set n for k times
+    #ALLOWS DUPLICATES
     result =[]
     for i in range(k):
         randNum= random.randrange(n)
         result.append(randNum)
     return result
-print(genRandSubSet(100,4))
+print(genRandSubSet(3,4))
 
 
 def random_subset(n: int, k: int):
 
+    if k > n:
+        return (f"Error: {k} is a subset size must be smaller than the set n")
+
     #Uniform random subset of size k from {0..n-1}
+    #(AKA LIKE A DECK OF CARDS, ALL CARDS HAVE EQUAL CHANCE OF BEING CHOSEN)
     #O(k) time / O(k) extra space.
 
-    changed = {}                    #maps index to value actually stored there
+    changed = {}  #maps index to value actually stored there
     for i in range(k):
         r = random.randrange(i, n)  #chooses partner to swap with i  
 
@@ -25,8 +31,9 @@ def random_subset(n: int, k: int):
         #perform the logical swap
         changed[i] = val_r
         changed[r] = val_i
+    print(changed)
 
     return [changed[i] for i in range(k)]
 
-print(random_subset(5, 3))   #tests
+print(random_subset(3, 4))   #tests
 print(random_subset(1000000, 5))  
