efad0a6658d72095d4954ea8729ae1c0556f836f
adalhub <adalteran@gmail.com>
2025-12-04 17:51:52 -0600

    feat: how to pass information between threads using queue

--- multi-threading/multithread.py
+++ multi-threading/multithread.py
@@ -1,20 +1,27 @@
 import time
 import threading
-
-def foo():
-    for _ in range(10):
+import queue
+def prodoucer(queue):
+    for i in range(10):
+        item = f"\\\ {i} ///" 
+        queue.put(item)
+        print(f"production of item {item}")
         time.sleep(1)
-        print("hello")
-
-
-def bar():
-    for _ in range(10):
-        time.sleep(1)
-        print("hi")
+    queue.put(None)
+def consumer(queue):
+    while True:
+        item= queue.get()
+        if not item:
+            break
+        print(f"CONSUMING item: {item}")
+        time.sleep(2)
 def main():
-    t1 = threading.Thread(target = foo)
-    t2 = threading.Thread(target = bar)
+    q = queue.Queue()
+    t1 = threading.Thread(target = prodoucer, args=(q,))
+    t2 = threading.Thread(target = consumer, args=(q,))
     t1.start()
     t2.start()
+    t1.join()
+    t2.join()
 if __name__ == "__main__":
     main()
